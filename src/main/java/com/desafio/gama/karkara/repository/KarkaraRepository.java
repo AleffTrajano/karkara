8f1470a4cfc7f80ace7bdb9755d142155a620005
Aleff Diniz <Aleff_diniz@hotmail.com>
2020-10-28 15:03:21 -0300

    Subindo a branch

---
+++ src/main/java/com/desafio/gama/karkara/repository/KarkaraRepository.java
@@ -0,0 +1,11 @@
+package com.desafio.gama.karkara.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KarkaraRepository {
+
+	
+}
